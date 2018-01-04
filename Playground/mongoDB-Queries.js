799d1e580cb74bff06c71b1cfaf57fdff24c9c2e
Aritra Banerjee <aritraWorkBanerjee@gmail.com>
2018-01-04 17:26:43 +0530

    Before Query by ID

--- Playground/mongoDB-Queries.js
+++ Playground/mongoDB-Queries.js
@@ -1,16 +1,18 @@
-const mongoose = require('./../Server/DB/mongoose');
-const Todo = require('./../Server/Models/ToDo');
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../Server/DB/mongoose');
+const {Todo} = require('./../Server/Models/ToDo');
 
 var id = "5a4b2b5ff5dc11842bf30685";
+if(!ObjectId.isValid(id)) {
+    return console.log('ID not valid');
+}
 
-Todo.find({_id : id}).then((todos) => {
-    console.log(todos)
-},(e) => {
-    console.log(e);
-});
-
-Todo.findOne({_id : id}).then((todo) => {
-    console.log(todo)
-},(e) => {
+Todo.findById(id).then((todo) => {
+    if(!todo) {
+        return console.log('Id not found');
+    }
+    console.log(todo);
+}).catch((e) => {
     console.log(e);
 });
